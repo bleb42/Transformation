f99e357e4b582c522e78410146be678b44318374
bleb42 <143229875+bleb42@users.noreply.github.com>
2023-09-24 20:06:49 +0200

    Update Rotation.cs

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Transform))]
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private float _rotationSpeed;
